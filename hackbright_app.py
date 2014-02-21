878c0272786a17d69cc9b4e63c9de9b5a8379b59
Hackbright Student <info@hackbrightacademy.com>
2014-02-21 11:55:48 -0800

    Exercise working through part 2 expansion 4

--- hackbright_app.py
+++ hackbright_app.py
@@ -11,6 +11,41 @@ def get_student_by_github(github):
 Student: %s %s
 Github account: %s"""%(row[0], row[1], row[2])
 
+def make_new_student(first_name,last_name,github):
+    query = """INSERT into Students (first_name, last_name, github) values (?, ?, ?)"""
+    DB.execute(query, (first_name, last_name, github))
+    CONN.commit()
+    print "Successfully added student: %s %s" % (first_name, last_name)
+
+def get_project_by_title(title):
+    query = """SELECT title, description, max_grade FROM Projects where title = ?"""
+    DB.execute(query, (title,))
+    row = DB.fetchone()
+    print """\
+Project: %s
+Descrription: %s
+Max Grade: %d"""%(row[0],row[1], row[2])
+
+def make_new_project(*args):
+    title = args[0]
+    description = ' '.join(args[1:-1])
+    max_grade = int(args[-1])
+    print title, description, max_grade
+    query = """INSERT into Projects (title, description, max_grade) VALUES (?, ?, ?);"""
+    DB.execute(query, (title, description, max_grade))
+    CONN.commit()
+    print "Successfully added project: %s" % title
+
+def get_project_grade_by_github(title, github):
+    query = """SELECT grade FROM Grades where project_title = ? and student_github = ?"""  
+    DB.execute(query, (title, github))
+    row = DB.fetchone()
+    print """\
+Project: %s
+UserName: %s
+Grade: %d"""% (title, github,row[0])
+
+
 def connect_to_db():
     global DB, CONN
     CONN = sqlite3.connect("hackbright.db")
@@ -29,6 +64,14 @@ def main():
             get_student_by_github(*args) 
         elif command == "new_student":
             make_new_student(*args)
+        elif command == "project":
+            get_project_by_title(*args)
+        elif command == "new_project":
+            make_new_project(*args)
+        elif command == "project_grade":
+            get_project_grade_by_github(*args)
+        else:
+            print ('Valid commands:  student, new_student, project, new_project')
 
     CONN.close()
 
